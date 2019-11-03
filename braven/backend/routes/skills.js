12847cb41d5e16ec9242fa9c625731f323944eba
shrivastavshubham34 <shrivastavshubham34@gmail.com>
2019-11-03 00:09:13 -0700

    added slot assignment logic

--- braven/backend/routes/skills.js
+++ braven/backend/routes/skills.js
@@ -2,6 +2,7 @@ class Tree {
     constructor(root) {
         this._root = root || null;
         this.level = 0;
+        this.parent = null;
     }
 
     _traverse(callback) {
@@ -19,18 +20,20 @@ class Tree {
     _addNode(value, parentValue) {
         const newNode = {
             value,
-            children: []
+            children: [],
         };
 
         if (this._root === null) {
             this._root = newNode;
             this._root.level = 0;
+            this._root.parent = null;
             return;
         }
 
         this._traverse((node) => {
             if (node.value === parentValue) {
                 newNode.level = node.level + 1;
+                newNode.parent = node;
                 node.children.push(newNode);
             }
         });
@@ -119,7 +122,234 @@ tree._addNode('Professor', 'Education');
 //     console.log(node.value);
 // });
 
-console.log(tree._search('Business'));
+//console.log(tree._search('Business'));
 
 //console.log(tree._displayLeafs('Software Engineering'));
+
+/*
+Slot allocation logic
+*/
+var studentSkills =
+    [{
+        "ID": 1,
+        "skill": 'DevOps',
+        "assigned": false
+    }, {
+        "ID": 2,
+        "skill": 'Software Engineering',
+        "assigned": false,
+    }, {
+        "ID": 3,
+        "skill": 'Full-stack',
+        "assigned": false,
+    }, {
+        "ID": 4,
+        "skill": 'Business',
+        "assigned": false,
+    }, {
+        "ID": 5,
+        "skill": 'Recruiting',
+        "assigned": false,
+    }, {
+        "ID": 6,
+        "skill": 'Physiologist',
+        "assigned": false,
+    }, {
+        "ID": 7,
+        "skill": 'Education',
+        "assigned": false,
+    }, {
+        "ID": 8,
+        "skill": 'Professor',
+        "assigned": false,
+    }, {
+        "ID": 9,
+        "skill": 'R&D',
+        "assigned": false,
+    }, {
+        "ID": 10,
+        "skill": 'Accounting',
+        "assigned": false,
+    }, {
+        "ID": 11,
+        "skill": 'Marketing',
+        "assigned": false,
+    }, {
+        "ID": 12,
+        "skill": 'Health',
+        "assigned": false,
+    }]
+
+var interviewerSkills =
+    [{
+        "ID": 1,
+        "skill": 'DevOps',
+        "studentID": []
+    }, {
+        "ID": 2,
+        "skill": 'Technology',
+        "studentID": [],
+    }, {
+        "ID": 3,
+        "skill": 'Software Engineering',
+        "studentID": [],
+    }, {
+        "ID": 4,
+        "skill": 'Human Resources',
+        "studentID": [],
+    }, {
+        "ID": 5,
+        "skill": 'Business',
+        "studentID": [],
+    }, {
+        "ID": 6,
+        "skill": 'Physiologist',
+        "studentID": [],
+    }, {
+        "ID": 7,
+        "skill": 'Professor',
+        "studentID": [],
+    }]
+
+var numberOfStudents = 12;
+
+var studentsAssigned = 0;
+
+var deepest = 3;
+
+//get level from tree 
+
+function getLevel(skill) {
+
+    return tree._search(skill).level;
+
+}
+
+function getParentSkill(skill) {
+    if (tree._search(skill).parent != null) {
+        //   console.log(tree._search(skill).parent.value);
+        return tree._search(skill).parent.value;
+    }
+    return null;
+}
+
+function setAllLevel() {
+
+    for (const interviewskill of interviewerSkills) {
+        interviewskill.level = getLevel(interviewskill.skill);
+    }
+
+    for (const studentSkill of studentSkills) {
+        studentSkill.level = getLevel(studentSkill.skill);
+    }
+}
+
+
+function moveLevelUp(deepest) {
+
+    // let leveldown = interviewerSkills.filter((data) => {
+    //     return data.level > deepest;
+    // })
+    // leveldown = leveldown.map((data) => {
+    //     data.level--;
+    //     data.skill = getParentSkill(data.skill)
+    //     return data;
+    // })
+    // const originalArray = interviewerSkills.filter((data) => {
+    //     return data.level <= deepest;
+    // })
+
+    // interviewerSkills = [...originalArray, ...leveldown];
+
+    for (var x in interviewerSkills) {
+        if (interviewerSkills[x].level > deepest) {
+            interviewerSkills[x].level--; //interviewskill.level != 0 && 
+            interviewerSkills[x].skill = getParentSkill(interviewerSkills[x].skill);
+            if (interviewerSkills[x].level > 1) {
+                interviewerSkills.push(interviewerSkills.splice(x, 1)[0]);
+
+            }
+        }
+    }
+    // for (const interviewskill of interviewerSkills) {
+    //     const newinterviewskill = interviewskill;
+
+
+    //     if (newinterviewskill.level > deepest) {
+    //         newinterviewskill.level--; //interviewskill.level != 0 && 
+    //         newinterviewskill.skill = getParentSkill(newinterviewskill.skill);
+    //     }
+    //     interviewerSkills.push(newinterviewskill);
+
+    // }
+
+    for (const studentSkill of studentSkills) {
+        if (studentSkill.level > deepest) {
+            studentSkill.level--; //interviewskill.level != 0 &&
+            studentSkill.skill = getParentSkill(studentSkill.skill);
+        }
+    }
+}
+
+setAllLevel();
+// moveLevelUp(2);
+// console.log(interviewerSkills);
+// console.log("-----------------");
+// console.log(studentSkills);
+// moveLevelUp(1);
+// console.log(interviewerSkills);
+// console.log("-----------------");
+// console.log(studentSkills);
+
+while (numberOfStudents != studentsAssigned) {
+    for (const studentSkill of studentSkills) {
+        if (!studentSkill.assigned) {
+            const sLevel = getLevel(studentSkill.skill); //3
+
+            if (sLevel == deepest) { // true
+
+                for (var interviewerSkill of interviewerSkills) {
+                    const iLevel = getLevel(interviewerSkill.skill);  // 3
+                    //node.children.push(newNode);
+                    if (interviewerSkill.skill == studentSkill.skill && iLevel == sLevel && interviewerSkill.studentID.length < 2) {
+                        // MATCH !!
+                        studentsAssigned++;
+                        studentSkill.assigned = true;
+                        interviewerSkill.studentID.push(studentSkill.ID);
+                        //console.log("interviewerSkill");
+                        //console.log(interviewerSkill);
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
+    //1
+
+    deepest--;
+    // if (deepest == 0) break;
+    // console.log("Deepest: " + deepest);
+    // console.log("----------int-------");
+    // console.log(interviewerSkills);
+    // console.log("--------std---------");
+    // console.log(studentSkills);
+    // console.log("+++++++++++++++++++");
+
+    moveLevelUp(deepest);  // [2,]
+
+    // console.log("After level up");
+    // console.log("Deepest: " + deepest);
+    // console.log("----------int-------");
+    // console.log(interviewerSkills);
+    // console.log("--------std---------");
+    // console.log(studentSkills);
+    // console.log("+++++++++++++++++++");
+
+
+}
+
+console.log(interviewerSkills);
+console.log("-----------------");
+console.log(studentSkills);
 
